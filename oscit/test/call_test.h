ac403b1523056ae8ced4aead41aa32805d1474d2
Gaspard Bucher <gaspard@teti.ch>
2009-05-06 11:35:57 +0200

    Moved makefile compilation to CMake.

--- oscit/test/call_test.h
+++ oscit/test/call_test.h
@@ -1,6 +1,7 @@
+#include <sstream>
 #include "test_helper.h"
 #include "oscit/mutex.h"
-#include <sstream>
+#include "mock/dummy_object.h"
 
 class CallTest : public TestHelper
 {  
@@ -10,7 +11,7 @@ public:
     DummyObject * evil = root.adopt(new DummyObject("Evil", 1.23));
     
     Call call("/Evil", Value(6.66));
-    
+
     assert_equal(1.23, evil->real());
     Value res = call.trigger(&root);
     assert_equal(6.66, res.r);
