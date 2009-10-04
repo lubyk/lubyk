fe9bdc990d507232b45d622047643c8cbafc81dd
Gaspard Bucher <gaspard@teti.ch>
2009-10-04 22:49:32 +0200

    Wrote some code for the Implementation of the ZeroConfBrowser on the iPhone.

---
+++ oscit/src/iphone/zeroconf_browser.mm
@@ -0,0 +1,130 @@
+#include "oscit/zeroconf.h"
+#import <Foundation/NSNetServices.h>
+#import <Foundation/Foundation.h>
+
+@interface NetServiceDelegate : NSObject {
+  oscit::ZeroConfBrowser *master_;
+  NSNetServiceBrowser *browser_;
+  bool running_;
+}
+
+- (id)initWithDelegate:(oscit::ZeroConfBrowser *)master serviceType:(const char *)service_type;
+- (void)stop;
+// NSNetServiceBrowser delegate methods
+- (void)netServiceBrowserWillSearch:(NSNetServiceBrowser *)browser;
+- (void)netServiceBrowserDidStopSearch:(NSNetServiceBrowser *)browser;
+- (void)netServiceBrowser:(NSNetServiceBrowser *)browser didNotSearch:(NSDictionary *)errorDict;
+- (void)netServiceBrowser:(NSNetServiceBrowser *)browser didFindService:(NSNetService *)aNetService moreComing:(BOOL)moreComing;
+- (void)netServiceBrowser:(NSNetServiceBrowser *)browser didRemoveService:(NSNetService *)aNetService moreComing:(BOOL)moreComing;
+
+// resolving host
+- (void)netServiceDidResolveAddress:(NSNetService *)service;
+- (void)netService:(NSNetService *)sender didNotResolve:(NSDictionary *)errorDict;
+@end
+
+@implementation NetServiceDelegate
+- (id)initWithDelegate:(oscit::ZeroConfBrowser *)master serviceType:(const char *)service_type {
+    if (self = [super init]) {
+    master_  = master;
+    browser_ = [[NSNetServiceBrowser alloc] init];
+    [browser_ setDelegate:self];
+    [browser_ searchForServicesOfType:[NSString stringWithCString:service_type encoding:NSUTF8StringEncoding] inDomain:@""];
+    running_ = YES;
+  }
+  return self;
+}
+
+- (void)stop {
+  if (running_) {
+    [browser_ stop];
+    running_ = NO;
+  }
+}
+// NSNetServiceBrowser delegate methods
+- (void)netServiceBrowserWillSearch:(NSNetServiceBrowser *)browser {
+  // ignore
+}
+
+- (void)netServiceBrowserDidStopSearch:(NSNetServiceBrowser *)browser {
+  // ignore
+}
+
+- (void)netServiceBrowser:(NSNetServiceBrowser *)browser didNotSearch:(NSDictionary *)errorDict {
+  // handle errors ....
+}
+
+- (void)netServiceWillResolve:(NSNetService *)service {
+  NSLog(@"Going to resolve %@\n", [service name]);
+}
+
+- (void)netServiceDidResolveAddress:(NSNetService *)service {
+  NSLog(@"\n\nResolved: %@ in %@:%i addresses: %i\n", [service name], [service hostName], [service port], [[service addresses] count]);
+  master_->add_device( [[service name] UTF8String],
+                       [[service hostName] UTF8String],
+                       [service port],
+                       false);
+}
+
+- (void)netService:(NSNetService *)service didNotResolve:(NSDictionary *)errorDict {
+  NSLog(@"Could not resolve %@!\n", [service name]);
+  // handle errors ...
+}
+
+- (void)netServiceBrowser:(NSNetServiceBrowser *)browser didFindService:(NSNetService *)service moreComing:(BOOL)moreComing {
+  NSLog(@"\n\nFound: %@ in %@:%i addresses: %i\n", [service name], [service hostName], [service port], [[service addresses] count]);
+  [service setDelegate:self];
+  [service resolveWithTimeout:0.0];
+}
+
+- (void)netServiceBrowser:(NSNetServiceBrowser *)browser didRemoveService:(NSNetService *)aNetService moreComing:(BOOL)moreComing {
+  master_->remove_device( [[aNetService name] UTF8String], moreComing);
+}
+
+-(BOOL) respondsToSelector:(SEL)selector {
+  NSLog(@"SELECTOR: %@\n", NSStringFromSelector(selector));
+  return [super respondsToSelector:selector];
+}
+
+-(void)dealloc {
+  [self stop];
+  [browser_ release];
+  [super dealloc];
+}
+
+@end
+
+namespace oscit {
+
+class ZeroConfBrowser::Implementation {
+public:
+  Implementation(ZeroConfBrowser *master) {
+    delegate_ = [[NetServiceDelegate alloc] initWithDelegate:master serviceType:master->service_type_.c_str()];
+  }
+  
+  ~Implementation() {
+    [delegate_ release];
+  }
+  
+  void stop() {
+    [delegate_ stop];
+  }
+
+  NetServiceDelegate *delegate_;
+};
+
+ZeroConfBrowser::ZeroConfBrowser(const char *service_type) : service_type_(service_type) {
+  impl_ = new ZeroConfBrowser::Implementation(this);
+}
+
+ZeroConfBrowser::~ZeroConfBrowser() {
+  delete impl_;
+}
+
+void ZeroConfBrowser::stop() {
+  impl_->stop();
+}
+
+} // oscit
+
+
+
---
+++ oscit/src/iphone/zeroconf_registration.cpp
@@ -0,0 +1,133 @@
+#include <stdio.h>			// For stdout, stderr
+#include <string.h>			// For strlen(), strcpy(), bzero()
+#include <errno.h>      // For errno, EINTR
+#include <time.h>
+
+#ifdef _WIN32
+#include <process.h>
+typedef	int	pid_t;
+#define	getpid	_getpid
+#define	strcasecmp	_stricmp
+#define snprintf _snprintf
+#else
+#include <sys/time.h>		// For struct timeval
+#include <unistd.h>     // For getopt() and optind
+#include <arpa/inet.h>	// For inet_addr()
+#endif
+
+#include <iostream>
+
+#include <dns_sd.h>     // zeroconf
+
+#include "oscit/thread.h"
+#include "oscit/zeroconf.h"
+
+
+namespace oscit {
+
+// Note: the select() implementation on Windows (Winsock2)
+//       fails with any timeout much larger than this
+#define LONG_TIME 100000000
+
+class ZeroConfRegistration::Implementation : public Thread {
+public:
+  Implementation(ZeroConfRegistration *master) : master_(master) {
+    start_thread<Implementation, &Implementation::registration>(this, NULL);
+  }
+
+  void register_service(DNSServiceRef service) {
+    // Run until break.
+    int dns_sd_fd = DNSServiceRefSockFD(service);
+    fd_set readfds;
+    struct timeval tv;
+    int result;
+
+    while (should_run()) {
+      FD_ZERO(&readfds);
+      FD_SET(dns_sd_fd, &readfds);
+      tv.tv_sec = LONG_TIME;
+      tv.tv_usec = 0;
+                      // highest fd in set + 1
+      result = select(dns_sd_fd+1, &readfds, (fd_set*)NULL, (fd_set*)NULL, &tv);
+      if (result > 0) {
+        DNSServiceErrorType err = kDNSServiceErr_NoError;
+        // Execute callback
+        if (FD_ISSET(dns_sd_fd, &readfds)) err = DNSServiceProcessResult(service);
+
+        if (err) {
+          // An error occured. Halt.
+          fprintf(stderr, "DNSServiceProcessResult returned %d\n", err);
+          quit();
+        }
+      }	else if (errno != EINTR) {
+        // Error.
+        fprintf(stderr, "ZeroConf error (%d %s)\n", errno, strerror(errno));
+        if (errno != EINTR) quit();
+      }
+    }
+  }
+
+  void registration(Thread *thread) {
+    //  release calling thread semaphore
+    thread->thread_ready();
+    
+    DNSServiceErrorType error;
+    DNSServiceRef service;
+
+    error = DNSServiceRegister(&service,
+      0,                    // no flags
+      0,                    // all network interfaces
+      master_->name_.c_str(),         // name
+      master_->service_type_.c_str(), // service type
+      "",                   // register in default domain(s)
+      NULL,                 // use default host name
+      htons(master_->port_),// port number
+      0,                    // length of TXT record
+      NULL,                 // no TXT record
+      Implementation::register_callback,  // callback function
+      (void*)master_);         // context
+
+    if (error == kDNSServiceErr_NoError) {
+      register_service(service);
+    } else {
+      fprintf(stderr,"Could not register service %s.%s on port %u (error %d)\n", master_->name_.c_str(), master_->service_type_.c_str(), master_->port_, error);//, strerror(errno));
+    }
+
+    DNSServiceRefDeallocate(service);
+  }
+
+  /** Callback called after registration. */
+  static void register_callback(DNSServiceRef ref,
+                                DNSServiceFlags flags,
+                                DNSServiceErrorType error,
+                                const char *name,
+                                const char *service_type,
+                                const char *host,
+                                void *context) {
+
+    if (error != kDNSServiceErr_NoError) {
+      fprintf(stderr, "DNSServiceRegister error (%d).\n", error);
+    } else {
+      ZeroConfRegistration *reg = (ZeroConfRegistration*)context;
+      reg->name_ = name;
+      reg->host_ = host;
+      reg->registration_done();
+    }
+  }
+  ZeroConfRegistration *master_;
+};
+
+
+ZeroConfRegistration::ZeroConfRegistration(const std::string &name, const char *service_type, uint16_t port) : name_(name), service_type_(service_type), port_(port) {
+  impl_ = new ZeroConfRegistration::Implementation(this);
+}
+
+ZeroConfRegistration::~ZeroConfRegistration() {
+  delete impl_;
+}
+
+void ZeroConfRegistration::stop() {
+  impl_->kill();
+}
+
+} // oscit
