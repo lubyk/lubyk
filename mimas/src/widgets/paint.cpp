77e6aa9752676e9728c6faaf3274fc5273718cfd
Gaspard Bucher <gaspard@teti.ch>
2009-12-30 10:57:36 +0100

    Made slider and pad look a little nicer (there are still some bugs on the sides).

--- mimas/src/widgets/paint.cpp
+++ mimas/src/widgets/paint.cpp
@@ -4,31 +4,80 @@
 
 #include "m_object_proxy.h"
 
-#define SLIDER_BORDER_WIDTH 1
-
+#define SLIDER_BORDER_WIDTH 2
+#define SLIDER_BORDER_RADIUS 6
 
 // =============================================
 // ==             MSlider                     ==
 // =============================================
 
+/**
+ * TODO: use a table lookup for the round_delta ?
+ */
+
 void MSlider::paint(Graphics &g) {
-  g.fillAll(Colours::black);
+  g.setColour(Colours::grey);
+  g.fillRoundedRectangle(
+    SLIDER_BORDER_WIDTH / 2,
+    SLIDER_BORDER_WIDTH / 2,
+    getWidth()-SLIDER_BORDER_WIDTH,
+    getHeight()-SLIDER_BORDER_WIDTH,
+    SLIDER_BORDER_RADIUS
+  );
   g.setColour(Colours::pink);
   if (slider_type_ == VerticalSliderType) {
     // vertical slider
-    int remote_pos = scaled_remote_value(getHeight());
-    g.fillRect(0, getHeight() - remote_pos, getWidth(), remote_pos);
+    int remote_pos = scaled_remote_value(getHeight()-2);
+    int h = getHeight() - remote_pos - SLIDER_BORDER_WIDTH / 2;
+    int round_delta = 0;
+    if (h < SLIDER_BORDER_RADIUS) {
+      round_delta = SLIDER_BORDER_RADIUS - h;
+    } else if (h > getHeight() - SLIDER_BORDER_RADIUS) {
+      round_delta = h - getHeight() - SLIDER_BORDER_RADIUS;
+    }
+    g.fillRoundedRectangle(
+      SLIDER_BORDER_WIDTH / 2,
+      h,
+      getWidth() - SLIDER_BORDER_WIDTH,
+      remote_pos,
+      SLIDER_BORDER_RADIUS
+    );
+    // remove rounded borders on top of slider
+    g.fillRect(
+      round_delta + SLIDER_BORDER_WIDTH / 2,
+      h,
+      getWidth() - SLIDER_BORDER_WIDTH - round_delta,
+      SLIDER_BORDER_RADIUS - round_delta
+    );
+    // small line on top of value
+    g.setColour(Colours::black);
+    g.drawLine(
+      round_delta + SLIDER_BORDER_WIDTH / 2,
+      h - 0.5f,
+      getWidth() - SLIDER_BORDER_WIDTH - round_delta,
+      h - 0.5f,
+      1.0f
+    );
     //if (remote_pos - position > 3 || remote_pos - position < -3) {
       // TODO: should be if dragged_ || last_drag ...
       // draw ghost
     //}
   } else {
     // horizontal slider
-    int remote_pos = scaled_remote_value(getWidth());
+    int remote_pos = scaled_remote_value(getWidth()-2);
     g.fillRect(0, 0, remote_pos, getHeight());
   }
-  g.setColour(Colours::darkgrey);
-  g.drawRect(0, 0, getWidth(), getHeight());
+  g.setColour(Colours::black);
+  g.drawRoundedRectangle(
+    SLIDER_BORDER_WIDTH / 2,
+    SLIDER_BORDER_WIDTH / 2,
+    getWidth() - SLIDER_BORDER_WIDTH,
+    getHeight() - SLIDER_BORDER_WIDTH,
+    SLIDER_BORDER_RADIUS,
+    SLIDER_BORDER_WIDTH
+  );
+
+  //g.drawRect(0, 0, getWidth(), getHeight());
 }
 
 // =============================================
@@ -61,4 +110,13 @@ void MPad::paint(Graphics& g) {
 
   g.setColour(Colours::black);
   g.drawEllipse(pos_x, pos_y, 2*radius, 2*radius, 2.0f);
+
+  g.setColour(Colours::black);
+  g.drawRect(
+    0,
+    0,
+    getWidth(),
+    getHeight(),
+    SLIDER_BORDER_WIDTH
+  );
 }
