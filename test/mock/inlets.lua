1c3a112aad4f044a41aecf9c4e835104773d6d79
Gaspard Bucher <gaspard@teti.ch>
2010-02-21 13:33:02 +0100

    Changed copyright year.

--- test/mock/inlets.lua
+++ test/mock/inlets.lua
@@ -1,7 +1,7 @@
 -- ==============================================================================
 --
 --  This file is part of the RUBYK project (http://rubyk.org)
---  Copyright (c) 2007-2009 by Gaspard Bucher - Buma (http://teti.ch).
+--  Copyright (c) 2007-2010 by Gaspard Bucher - Buma (http://teti.ch).
 --
 -- ------------------------------------------------------------------------------
 --
