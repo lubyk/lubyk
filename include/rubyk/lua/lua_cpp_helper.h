c5987b7ccf15446f8e41faede5967d8e023f827e
Gaspard Bucher <gaspard@teti.ch>
2010-03-07 13:17:05 +0100

    Including latest version of oscit submodule and missing source files.

---
+++ include/rubyk/lua/lua_cpp_helper.h
@@ -0,0 +1,141 @@
+
+#ifndef DOXY_GENERATOR_LIB_DOXY_GENERATOR_INCLUDE_LUA_DOXY_HELPER_H_
+#define DOXY_GENERATOR_LIB_DOXY_GENERATOR_INCLUDE_LUA_DOXY_HELPER_H_
+
+#include <stdlib.h> // malloc
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+// We need C linkage because lua lib is compiled as C code
+#include "lua.h"
+#include "lauxlib.h"
+
+#ifdef __cplusplus
+}
+#endif
+/** ======================================== lua_pushclass          */
+
+/** Push a custom type on the stack.
+ * Since the value is passed as a pointer, we assume it has been created
+ * using 'new' and Lua can safely call delete when it needs to garbage-
+ * -collect it.
+ */
+template<class T>
+void lua_pushclass(lua_State *L, T *ptr, const char *type_name) {
+  T **userdata = (T**)lua_newuserdata(L, sizeof(T*));
+  *userdata = ptr;
+
+  // the userdata is now on top of the stack
+
+  // set metatable (contains methods)
+  luaL_getmetatable(L, type_name);
+  lua_setmetatable(L, -2);
+}
+
+/** Push a custom type on the stack.
+ * Since the value is passed by value, we have to allocate a copy
+ * using 'new' so that Lua can keep it.
+ */
+template<class T>
+void lua_pushclass(lua_State *L, T &val, const char *type_name) {
+  T *val_ptr = new T(val);
+  lua_pushclass<T>(L, val_ptr, type_name);
+}
+
+/** ======================================== DubArgPointer */
+
+/** This class is a helper to provide pointer to data from
+ * Lua to C (but maybe it's not a good idea).
+ */
+template<class T>
+class DubArgPointer {
+public:
+  DubArgPointer() : data(NULL) {}
+
+  ~DubArgPointer() {
+    if (data) free(data);
+  }
+
+  // TODO: we should have a hint on required sizes !
+  T *operator()(lua_State *L, int index) {
+    if (!lua_istable(L, index)) throw 1;
+
+    size_t size = lua_objlen(L, index);
+    if (size == 0) return NULL;
+
+    data = (T*)malloc(size * sizeof(T));
+    if (!data) throw 1;
+
+    for(size_t i=0; i < size; ++i) {
+      data[i] = get_value_at(L, index, i+1);
+    }
+    return data;
+  }
+
+private:
+  T get_value_at(lua_State *L, int table_index, int index) {
+    lua_pushinteger(L, index + 1);
+    lua_gettable(L, index);
+    T value = luaL_checknumber(L, -1);
+    lua_pop(L, 1);
+    return value;
+  }
+
+  T *data;
+};
+
+/** ======================================== is_userdata */
+
+inline bool is_userdata(lua_State *L, int index, const char *tname) {
+  void *p = lua_touserdata(L, index);
+  if (p != NULL) {  /* value is a userdata? */
+    if (lua_getmetatable(L, index)) {  /* does it have a metatable? */
+      lua_getfield(L, LUA_REGISTRYINDEX, tname);  /* get correct metatable */
+      if (lua_rawequal(L, -1, -2)) {  /* does it have the correct mt? */
+        lua_pop(L, 2);  /* remove both metatables */
+        // type match
+        return true;
+      }
+    }
+  }
+  // type does not match
+  return false;
+}
+
+/** ======================================== register_constants */
+
+
+typedef struct lua_constants_Reg {
+  const char *name;
+  double constant;
+} lua_constants_Reg;
+
+inline int libsize (const lua_constants_Reg *l) {
+  int size = 0;
+  for (; l->name; l++) size++;
+  return size;
+}
+
+inline void register_constants(lua_State *L, const char *name_space, const lua_constants_Reg *l) {
+  if (name_space) {
+    /* compute size hint for new table. */
+    int size = libsize(l);
+
+    /* try global variable (and create one if it does not exist) */
+    if (luaL_findtable(L, LUA_GLOBALSINDEX, name_space, size) != NULL)
+      luaL_error(L, "name conflict for module " LUA_QS, name_space);
+
+    /* found name_space in global index ==> stack -1 */
+  }
+  for (; l->name; l++) {
+    /* push each constant into the name_space (stack position = -1)*/
+    lua_pushnumber(L, l->constant);
+    lua_setfield(L, -2, l->name);
+  }
+  /* pop name_space */
+  lua_pop(L, 1);
+}
+
+
+#endif // DOXY_GENERATOR_LIB_DOXY_GENERATOR_INCLUDE_LUA_DOXY_HELPER_H_
---
+++ include/rubyk/support/draw.h
@@ -0,0 +1,115 @@
+/*
+  ==============================================================================
+
+   This file is part of the RUBYK project (http://rubyk.org)
+   Copyright (c) 2007-2010 by Gaspard Bucher - Buma (http://teti.ch).
+
+  ------------------------------------------------------------------------------
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+
+  ==============================================================================
+*/
+
+#ifndef RUBYK_INCLUDE_RUBYK_SUPPORT_DRAW_H_
+#define RUBYK_INCLUDE_RUBYK_SUPPORT_DRAW_H_
+
+#include "opencv/cv.h"
+
+/** Main namespace for rubyk.
+ */
+namespace rk {
+
+/** Draw a CV_8UC3  matrix (setting colors from matrix content).
+ * Obsolete, use textures instead (@ref makeTexture, @ref drawTexture)
+ * @param mat matrix containing the pixel colors
+ * @param start_x left border of the rectangle in which to draw the matrix
+ * @param start_y bottom of the rectangle in which to draw the matrix
+ * @param end_x right border of the rectangle in which to draw the matrix
+ * @param end_y top border of the rectangle in which to draw the matrix
+ * @param z_mul use luminosity as z index (do not use: will be removed)
+ */
+void drawMatrix(const cv::Mat &mat, float start_x, float start_y, float end_x, float end_y, float alpha = 1.0, float z_mul = 0, bool strips = false);
+
+/** Create a texture from a matrix.
+ * @param mat matrix containing the data to be projected on surfaces
+ * @param tex a texture id returned by glGenTextures
+ */
+void makeTexture(const cv::Mat &mat, int tex);
+
+/** Draw a given texture on a square rectangle.
+ * Do not forget to set a color before drawing the texture (usually full white).
+ * @param tex a texture id returned by glGenTextures on which matrix data has been projected with @ref makeTexture
+ * @param start_x left border of the rectangle in which to draw the texture
+ * @param start_y bottom of the rectangle in which to draw the texture
+ * @param end_x right border of the rectangle in which to draw the texture
+ * @param end_y top border of the rectangle in which to draw the texture
+ */
+void drawTexture(int tex, float start_x, float start_y, float end_x, float end_y);
+
+} // rk
+
+/** Additions to cv namespace
+ */
+namespace cv {
+
+/** Get value at the given position (eventually specifying the channel).
+ * @param x column
+ * @param y row
+ * @param c channel (not used for 1 channel images)
+ * @return float value at the position (returns 0 for out-of-bound values)
+ */
+inline float rk_mat_at(const cv::Mat &m, int x, int y, int c=0) {
+  if ( y < 0 || y >= m.rows || x < 0 || x >= m.cols || c < 0 || c >= m.channels()) return 0;
+  switch (m.depth()) {
+    case CV_8U:  return ((const unsigned char* )(m.data + m.step*y))[x];
+    case CV_32F: return ((const float*)(m.data + m.step*y))[x];
+    default:
+      return 0; // FIXME get other types
+  }
+}
+} // cv
+
+/** Additions to glu namespace that uses our Matrix class
+ */
+namespace glu {
+/** Build a mipmap from a Matrix.
+ * @param mat matrix (image) containing the data (must be a CV_8UC3)
+ */
+void Build2DMipmaps(const cv::Mat &mat);
+} // glu
+
+
+/** Additions to gl namespace that uses our Matrix class
+ */
+namespace gl {
+
+/** Draw matrix pixels.
+ * @param mat matrix (image) containing the data (must be a CV_8UC3)
+ */
+void DrawPixels(const cv::Mat &mat);
+
+/** Load a matrix into the current texture.
+ * @param mat the matrix that will be drawn on the texture.
+ */
+void TexImage(const cv::Mat &mat);
+
+} // gl
+
+#endif // RUBYK_INCLUDE_RUBYK_SUPPORT_DRAW_H_
