b7be31574254c276479ebc8331f2269b92cc0ddb
Gaspard Bucher <gaspard@teti.ch>
2011-02-15 00:03:26 +0100

    Basic environment for nodes.

---
+++ modules/lk/lua/lk/Environment.lua
@@ -0,0 +1,33 @@
+--[[------------------------------------------------------
+
+  lk.Environment
+  --------------
+
+  Run code in a new environment with read-only access to the
+  global namespace.
+
+--]]------------------------------------------------------
+
+local lib = {}
+lk.Environment = lib
+
+setmetatable(lib, {
+  -- new method
+ __call = function(table, code_str)
+  local code = assert(loadstring(code_str))
+  -- new environment
+  local env  = {}
+  setmetatable(env, lib)
+  setfenv(code, env)
+  return code
+end})
+
+-- metatable for the new global env in each
+-- node
+function lib.__index(env, name)
+  -- if the variable exists in original _G,
+  -- cache locally
+  local gvar = _G[name]
+  rawset(env, name, gvar)
+  return gvar
+end
