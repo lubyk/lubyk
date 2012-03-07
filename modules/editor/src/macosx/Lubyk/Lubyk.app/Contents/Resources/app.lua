232f1b186876e57270ca28cb1e9be31606bdd1f3
Gaspard Bucher <gaspard@teti.ch>
2012-03-07 16:01:58 +0100

    Changing editor into a submodule

--- modules/editor/src/macosx/Lubyk/Lubyk.app/Contents/Resources/app.lua
+++
@@ -1,8 +0,0 @@
--- This should work without needing lubyk to be installed
-if arg[1] then
-  package.path  = arg[1] .. '/?.lua;' .. package.path
-  package.cpath = arg[1] .. '/?.so;'  .. package.cpath
-end
-require 'lubyk'
-app = editor.Application(arg)
-run()
