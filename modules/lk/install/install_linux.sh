7a7dde5b6282bf58c872bcdeb4d3dd1327449644
Gaspard Bucher <gaspard@teti.ch>
2012-03-15 16:23:43 +0100

    Working on the build system for linux.

--- modules/lk/install/install_linux.sh
+++ modules/lk/install/install_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Must be run from the git/lubyk directory.
+# Must be run from the build directory.
 mkdir -p /usr/local/share/lua/5.1/
 mkdir -p /usr/local/bin/
 cp deploy/linux/lib/lubyk/lubyk_linux.lua /usr/local/share/lua/5.1/lubyk.lua
